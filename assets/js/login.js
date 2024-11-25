3851ff5799964f0c8a5f88ec7aaf7fbd13553a08
RaffiRmdhn <klsterbuka@gmail.com>
2024-11-25 15:58:05 +0700

    feat: Login localStorage

---
+++ assets/js/login.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const buttonLogin = document.getElementById("btn-login-masuk");
+
+  if (buttonLogin) {
+    buttonLogin.addEventListener("click", function (e) {
+      e.preventDefault();
+      const username = document.getElementById("loginUsername")?.value;
+      const password = document.getElementById("loginPassword")?.value;
+
+      if (!username) {
+        alert("Invalid username");
+        return;
+      }
+
+      if (password != "123") {
+        alert("Wrong password!")
+        return;
+      }
+
+      localStorage.setItem("login-username", username);
+      alert("Login successfull!");
+      window.location.reload();
+    });
+  }
+
+  const loginUsername = localStorage.getItem("login-username");
+  if (loginUsername) {
+    const buttonMasuk = document.getElementById("button-masuk")
+    const buttonKeluar = document.getElementById("button-keluar")
+    buttonMasuk.classList.add("d-none")
+    buttonKeluar.classList.remove("d-none")
+    document.getElementById("username-show").textContent = loginUsername
+  }
+
+  const buttonKeluar = document.getElementById("button-keluar");
+  buttonKeluar.addEventListener("click", function (e) {
+    e.preventDefault();
+    localStorage.removeItem("login-username")
+    window.location.reload();
+  })
+});
